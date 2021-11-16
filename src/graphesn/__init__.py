e94ebcb3b70c49da74b61937cd7d0d9f5c09622b
Domenico Tortorella <dom.tortorella@gmail.com>
2021-11-16 14:50:34 +0100

    Updated exports

--- src/graphesn/__init__.py
+++ src/graphesn/__init__.py
@@ -1,4 +1,6 @@
+from .data import TemporalData, DynamicData, DynamicBatch
 from .readout import Readout
 from .reservoir import initializer, StaticGraphReservoir, DynamicGraphReservoir
 
-__all__ = ['StaticGraphReservoir', 'DynamicGraphReservoir', 'initializer', 'Readout']
+__all__ = ['StaticGraphReservoir', 'DynamicGraphReservoir', 'initializer', 'Readout', 'TemporalData', 'DynamicData',
+           'DynamicBatch']
