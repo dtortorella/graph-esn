3477fd755c80fa3978977ef7595a3ab964c66f99
Domenico Tortorella <dom.tortorella@gmail.com>
2021-11-30 16:39:54 +0100

    Added some datasets from pytorch geometric temporal

--- src/graphesn/dataset.py
+++ src/graphesn/dataset.py
@@ -1,13 +1,17 @@
 import os.path
-from typing import Union, Iterable, List
+from typing import Union, Iterable, List, Optional
 
 import numpy as np
 import pandas as pd
 import torch
 from torch_geometric.data import download_url, extract_zip
+from torch_geometric_temporal import TwitterTennisDatasetLoader, PedalMeDatasetLoader, WikiMathsDatasetLoader
+from torch_geometric_temporal.dataset import ChickenpoxDatasetLoader
 from tqdm import tqdm
 
-from graphesn.data import DynamicData
+from graphesn.data import DynamicData, TemporalData
+
+__all__ = ['TGKDataset', 'chickenpox_dataset', 'twitter_tennis_dataset', 'pedalme_dataset', 'wiki_maths_dataset']
 
 
 class TGKDataset:
@@ -79,3 +83,100 @@ class TGKDataset:
 
     def __repr__(self):
         return f'{self.name}({len(self.data)})'
+
+
+def chickenpox_dataset(target_lags: int = 8, feature_lags: bool = True) -> TemporalData:
+    """
+    Chickenpox temporal graph dataset
+
+    See:
+    Chickenpox Cases in Hungary: a Benchmark Dataset for Spatiotemporal Signal Processing with Graph Neural Networks.
+    Benedek Rozemberczki, Paul Scherer, Oliver Kiss, Rik Sarkar, Tamas Ferenci (2021).
+    https://arxiv.org/abs/2102.08100
+
+    :param target_lags: Prediction target time-steps lag
+    :param feature_lags: If true, include lagged features (default true)
+    :return: A single temporal graph
+    """
+    loader = ChickenpoxDatasetLoader()
+    dataset = loader.get_dataset(lags=target_lags)
+    return TemporalData(temporal_keys=['x', 'y'],
+                        edge_index=torch.from_numpy(dataset.edge_index),
+                        x=torch.stack([torch.from_numpy(x.astype('float32')) if feature_lags else torch.from_numpy(
+                            x[:, 0].astype('float32')).unsqueeze(dim=-1) for x in dataset.features], dim=0),
+                        y=torch.stack(
+                            [torch.from_numpy(y.astype('float32')).unsqueeze(dim=-1) for y in dataset.targets], dim=0))
+
+
+def twitter_tennis_dataset(event_id: str = 'rg17', num_nodes: Optional[int] = None, feature_mode: str = 'encoded',
+                           target_offset: int = 1) -> DynamicData:
+    """
+    Twitter tennis dynamic graph dataset
+
+    See:
+    F. Béres, R. Pálovics, A. Oláh, et al. Temporal walk based centrality metric for graph streams.
+    Applied Network Science 3, 32 (2018). https://doi.org/10.1007/s41109-018-0080-5
+
+    :param event_id: Name of the event, 'rg17' or 'uo17'
+    :param num_nodes: Select top nodes (optional, default all)
+    :param feature_mode: Can be None for raw features, or 'encoded' for one hot encoding, or 'diagonal' for identity matrix
+    :param target_offset: Prediction off-set for node labels (default 1)
+    :return: A single dynamic graph
+    """
+    loader = TwitterTennisDatasetLoader(event_id=event_id, N=num_nodes, feature_mode=feature_mode,
+                                        target_offset=target_offset)
+    dataset = loader.get_dataset()
+    return DynamicData(edge_index=[torch.from_numpy(edge_index) for edge_index in dataset.edge_indices],
+                       edge_weight=[torch.from_numpy(edge_weight.astype('float32')) for edge_weight in
+                                    dataset.edge_weights],
+                       x=torch.stack([torch.from_numpy(x.astype('float32')) for x in dataset.features], dim=0),
+                       y=torch.stack(
+                           [torch.from_numpy(y.astype('float32')).unsqueeze(dim=-1) for y in dataset.targets], dim=0))
+
+
+def pedalme_dataset(target_lags: int = 8, feature_lags: bool = True) -> TemporalData:
+    """
+    PedalMe temporal graph dataset
+
+    See:
+    B. Rozemberczki, P. Scherer, Y. He, et al. (2021). PyTorch Geometric Temporal: Spatiotemporal Signal Processing with Neural Machine Learning Models.
+    Proceedings of the 30th ACM International Conference on Information & Knowledge Management (CIKM '21), pp. 4564–4573.
+    https://doi.org/10.1145/3459637.3482014
+
+    :param target_lags: Prediction target time-steps lag
+    :param feature_lags: If true, include lagged features (default true)
+    :return: A single temporal graph
+    """
+    loader = PedalMeDatasetLoader()
+    dataset = loader.get_dataset(lags=target_lags)
+    return TemporalData(temporal_keys=['x', 'y'],
+                        edge_index=torch.from_numpy(dataset.edge_index),
+                        edge_weight=torch.from_numpy(dataset.edge_weight.astype('float32')),
+                        x=torch.stack([torch.from_numpy(x.astype('float32')) if feature_lags else torch.from_numpy(
+                            x[:, 0].astype('float32')).unsqueeze(dim=-1) for x in dataset.features], dim=0),
+                        y=torch.stack(
+                            [torch.from_numpy(y.astype('float32')).unsqueeze(dim=-1) for y in dataset.targets], dim=0))
+
+
+def wiki_maths_dataset(target_lags: int = 8, feature_lags: bool = True) -> TemporalData:
+    """
+    Wiki Maths temporal graph dataset
+
+    See:
+    B. Rozemberczki, P. Scherer, Y. He, et al. (2021). PyTorch Geometric Temporal: Spatiotemporal Signal Processing with Neural Machine Learning Models.
+    Proceedings of the 30th ACM International Conference on Information & Knowledge Management (CIKM '21), pp. 4564–4573.
+    https://doi.org/10.1145/3459637.3482014
+
+    :param target_lags: Prediction target time-steps lag
+    :param feature_lags: If true, include lagged features (default true)
+    :return: A single temporal graph
+    """
+    loader = WikiMathsDatasetLoader()
+    dataset = loader.get_dataset(lags=target_lags)
+    return TemporalData(temporal_keys=['x', 'y'],
+                        edge_index=torch.from_numpy(dataset.edge_index),
+                        edge_weight=torch.from_numpy(dataset.edge_weight.astype('float32')),
+                        x=torch.stack([torch.from_numpy(x.astype('float32')) if feature_lags else torch.from_numpy(
+                            x[:, 0].astype('float32')).unsqueeze(dim=-1) for x in dataset.features], dim=0),
+                        y=torch.stack(
+                            [torch.from_numpy(y.astype('float32')).unsqueeze(dim=-1) for y in dataset.targets], dim=0))
